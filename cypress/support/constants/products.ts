a244fb9f6c620737b3156388cb32590c92d5c7de
Ida Jankowska <ida.jankowska@onet.pl>
2026-04-29 19:06:33 +0200

    feat: add products constants

---
+++ cypress/support/constants/products.ts
@@ -0,0 +1,10 @@
+export const PRODUCTS = {
+  backpack: {
+    id: "sauce-labs-backpack",
+    name: "Sauce Labs Backpack",
+  },
+  fleeceJacket: {
+    id: "sauce-labs-fleece-jacket",
+    name: "Sauce Labs Fleece Jacket",
+  },
+};
