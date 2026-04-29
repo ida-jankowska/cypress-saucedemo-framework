8f626a24f504443eec61e5cd79bac38b1e131cb1
Ida Jankowska <ida.jankowska@onet.pl>
2026-04-29 16:45:44 +0200

    feat: add custom login command

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,1 +1,19 @@
+/// <reference types="cypress" />
+
+import { LoginLocators } from "../pages/login/login.locators";
+
+Cypress.Commands.add("login", (user: string, password: string) => {
+  cy.get(LoginLocators.usernameInput).type(user);
+  cy.get(LoginLocators.passwordInput).type(password);
+  cy.get(LoginLocators.loginButton).click();
+});
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      login(user: string, password: string): Chainable<void>;
+    }
+  }
+}
+
 export {};
