d1fb95ab2467375226af80c00ae7d5add380c642
Ida Jankowska <ida.jankowska@onet.pl>
2026-04-29 19:07:23 +0200

    feat: add loginAsStandardUser command

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
-import { LoginLocators } from "../pages/login/login.locators";
+import { LoginLocators } from "../ui/pages/login/login.locators";
+import { routes } from "./constants/routes";
 
 Cypress.Commands.add("login", (user: string, password: string) => {
   cy.get(LoginLocators.usernameInput).type(user);
@@ -8,10 +9,18 @@ Cypress.Commands.add("login", (user: string, password: string) => {
   cy.get(LoginLocators.loginButton).click();
 });
 
+Cypress.Commands.add("loginAsStandardUser", () => {
+  cy.fixture("users").then((users) => {
+    cy.visit(routes.login);
+    cy.login(users.usernames.standard, users.passwords.valid);
+  });
+});
+
 declare global {
   namespace Cypress {
     interface Chainable {
       login(user: string, password: string): Chainable<void>;
+      loginAsStandardUser(): Chainable<void>;
     }
   }
 }
